7f97ab4a1de3017615a29ee8d7e14a63412b1a66
AnastasyaChe <bepin@yandex.ru>
2021-03-12 09:49:16 +0300

    'hw7'

---
+++ less7/ex3/IDomainObject.php
@@ -0,0 +1,6 @@
+<?php 
+interface IDomainObject
+{
+    public function getId();
+
+}
---
+++ less7/ex3/Product.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types = 1);
+
+class Product implements IDomainObject
+{
+    private $id;
+    private $name;
+    private $price;
+
+    public function __construct(int $id, string $name, float $price)
+    {
+        $this->id = $id;
+        $this->name = $name;
+        $this->price = $price;
+    }
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function getPrice(): float
+    {
+        return $this->price;
+    }
+
+    public function toArray(): array
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'price' => $this->price,
+        ];
+    }
+}
